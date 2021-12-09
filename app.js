d5aaab2485ffa3b407dadb76da1161a7fa24313d
Togo01 <togoyu01@gmail.com>
2021-12-09 14:04:33 +0800

    add google analytics

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-8JD2SP0CDZ');
+
 $(function(){
     if (navigator.userAgent.includes("Tesla")) {
 
